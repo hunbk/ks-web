0eaac9f86cae84c25e73579a4d865daf74e6a4cf
Byeonghun Kim <shyun823@gmail.com>
2020-05-06 01:42:45 +0900

    Skill 객체 -> View 연결

--- src/main/java/com/byeonghun/app/controller/IndexController.java
+++ src/main/java/com/byeonghun/app/controller/IndexController.java
@@ -2,9 +2,11 @@ package com.byeonghun.app.controller;
 
 import com.byeonghun.app.entity.Interests;
 import com.byeonghun.app.entity.Person;
+import com.byeonghun.app.entity.Skill;
 import com.byeonghun.app.entity.SocialMedia;
 import com.byeonghun.app.repository.InterestsRepository;
 import com.byeonghun.app.repository.PersonRepository;
+import com.byeonghun.app.repository.SkillRepository;
 import com.byeonghun.app.repository.SocialMediaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,11 +20,13 @@ public class IndexController {
     private final PersonRepository personRepository;
     private final SocialMediaRepository socialMediaRepository;
     private final InterestsRepository interestsRepository;
+    private final SkillRepository skillRepository;
 
-    public IndexController(PersonRepository personRepository, SocialMediaRepository socialMediaRepository, InterestsRepository interestsRepository) {
+    public IndexController(PersonRepository personRepository, SocialMediaRepository socialMediaRepository, InterestsRepository interestsRepository, SkillRepository skillRepository) {
         this.personRepository = personRepository;
         this.socialMediaRepository = socialMediaRepository;
         this.interestsRepository = interestsRepository;
+        this.skillRepository = skillRepository;
     }
 
     @GetMapping("/")
@@ -30,6 +34,7 @@ public class IndexController {
         Person person = personRepository.findAll().stream().findFirst().orElse(null); model.addAttribute("person", person);
         SocialMedia socialMedia = socialMediaRepository.findAll().stream().findFirst().orElse(null); model.addAttribute("socialMedia",socialMedia);
         List<Interests> interests = interestsRepository.findAll(); model.addAttribute("interests", interests);
+        List<Skill> skills = skillRepository.findAll(); model.addAttribute("skills", skills);
         return "resume";
     }
 }
