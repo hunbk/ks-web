3105695fb2e9ace7816381428d38f4f64cd66b3a
Byeonghun Kim <shyun823@gmail.com>
2020-05-06 01:34:36 +0900

    Interests Dummy 데이터 작성

--- src/main/java/com/byeonghun/app/AppApplication.java
+++ src/main/java/com/byeonghun/app/AppApplication.java
@@ -1,7 +1,9 @@
 package com.byeonghun.app;
 
+import com.byeonghun.app.entity.Interests;
 import com.byeonghun.app.entity.Person;
 import com.byeonghun.app.entity.SocialMedia;
+import com.byeonghun.app.repository.InterestsRepository;
 import com.byeonghun.app.repository.PersonRepository;
 import com.byeonghun.app.repository.SocialMediaRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -13,10 +15,12 @@ public class AppApplication implements CommandLineRunner {
 
 	private final PersonRepository personRepository;
 	private final SocialMediaRepository socialMediaRepository;
+	private final InterestsRepository interestsRepository;
 
-	public AppApplication(PersonRepository personRepository, SocialMediaRepository socialMediaRepository) {
+	public AppApplication(PersonRepository personRepository, SocialMediaRepository socialMediaRepository, InterestsRepository interestsRepository) {
 		this.personRepository = personRepository;
 		this.socialMediaRepository = socialMediaRepository;
+		this.interestsRepository = interestsRepository;
 	}
 
 	public static void main(String[] args) {
@@ -27,5 +31,7 @@ public class AppApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		personRepository.save(new Person("김병훈","백엔드 개발자","hello@test.com","010-1234-5678"));
 		socialMediaRepository.save(new SocialMedia("anonymous","","","anonymous"));
+		interestsRepository.save(new Interests("곤충사육"));
+		interestsRepository.save(new Interests("낚시"));
 	}
 }
