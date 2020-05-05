628a38a28cf88d68c88ebee9ba38f42f30e97aa0
Byeonghun Kim <shyun823@gmail.com>
2020-05-06 01:42:25 +0900

    Skill Dummy 데이터 작성

--- src/main/java/com/byeonghun/app/AppApplication.java
+++ src/main/java/com/byeonghun/app/AppApplication.java
@@ -2,9 +2,11 @@ package com.byeonghun.app;
 
 import com.byeonghun.app.entity.Interests;
 import com.byeonghun.app.entity.Person;
+import com.byeonghun.app.entity.Skill;
 import com.byeonghun.app.entity.SocialMedia;
 import com.byeonghun.app.repository.InterestsRepository;
 import com.byeonghun.app.repository.PersonRepository;
+import com.byeonghun.app.repository.SkillRepository;
 import com.byeonghun.app.repository.SocialMediaRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,11 +18,13 @@ public class AppApplication implements CommandLineRunner {
 	private final PersonRepository personRepository;
 	private final SocialMediaRepository socialMediaRepository;
 	private final InterestsRepository interestsRepository;
+	private final SkillRepository skillRepository;
 
-	public AppApplication(PersonRepository personRepository, SocialMediaRepository socialMediaRepository, InterestsRepository interestsRepository) {
+	public AppApplication(PersonRepository personRepository, SocialMediaRepository socialMediaRepository, InterestsRepository interestsRepository, SkillRepository skillRepository) {
 		this.personRepository = personRepository;
 		this.socialMediaRepository = socialMediaRepository;
 		this.interestsRepository = interestsRepository;
+		this.skillRepository = skillRepository;
 	}
 
 	public static void main(String[] args) {
@@ -33,5 +37,9 @@ public class AppApplication implements CommandLineRunner {
 		socialMediaRepository.save(new SocialMedia("anonymous","","","anonymous"));
 		interestsRepository.save(new Interests("곤충사육"));
 		interestsRepository.save(new Interests("낚시"));
+		skillRepository.save(new Skill("JAVA", 50));
+		skillRepository.save(new Skill("HTML", 30));
+		skillRepository.save(new Skill("JS", 40));
+		skillRepository.save(new Skill("CSS", 20));
 	}
 }
