dcf099fabfa405c9ff09e0f8b2b515469d223d95
Byeonghun Kim <shyun823@gmail.com>
2020-05-06 01:04:40 +0900

    Person Dummy 데이터 작성

--- src/main/java/com/byeonghun/app/AppApplication.java
+++ src/main/java/com/byeonghun/app/AppApplication.java
@@ -1,13 +1,26 @@
 package com.byeonghun.app;
 
+import com.byeonghun.app.entity.Person;
+import com.byeonghun.app.repository.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppApplication {
+public class AppApplication implements CommandLineRunner {
+
+	private final PersonRepository personRepository;
+
+	public AppApplication(PersonRepository personRepository) {
+		this.personRepository = personRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		personRepository.save(new Person("김병훈","백엔드 개발자","hello@test.com","010-1234-5678"));
+	}
 }
