9f7f611f6895e75854d435c8e5e0982252b586b8
Byeonghun Kim <shyun823@gmail.com>
2020-05-06 01:20:31 +0900

    SocialMedia Dummy 데이터 작성

--- src/main/java/com/byeonghun/app/AppApplication.java
+++ src/main/java/com/byeonghun/app/AppApplication.java
@@ -1,7 +1,9 @@
 package com.byeonghun.app;
 
 import com.byeonghun.app.entity.Person;
+import com.byeonghun.app.entity.SocialMedia;
 import com.byeonghun.app.repository.PersonRepository;
+import com.byeonghun.app.repository.SocialMediaRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,9 +12,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppApplication implements CommandLineRunner {
 
 	private final PersonRepository personRepository;
+	private final SocialMediaRepository socialMediaRepository;
 
-	public AppApplication(PersonRepository personRepository) {
+	public AppApplication(PersonRepository personRepository, SocialMediaRepository socialMediaRepository) {
 		this.personRepository = personRepository;
+		this.socialMediaRepository = socialMediaRepository;
 	}
 
 	public static void main(String[] args) {
@@ -22,5 +26,6 @@ public class AppApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		personRepository.save(new Person("김병훈","백엔드 개발자","hello@test.com","010-1234-5678"));
+		socialMediaRepository.save(new SocialMedia("anonymous","","","anonymous"));
 	}
 }
