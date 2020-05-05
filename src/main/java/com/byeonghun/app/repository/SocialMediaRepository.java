fbb06392b24dd16521e38241d20a4898ae079ef8
Byeonghun Kim <shyun823@gmail.com>
2020-05-06 01:19:31 +0900

    SocialMediaRepository 생성

---
+++ src/main/java/com/byeonghun/app/repository/SocialMediaRepository.java
@@ -0,0 +1,7 @@
+package com.byeonghun.app.repository;
+
+import com.byeonghun.app.entity.SocialMedia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SocialMediaRepository extends JpaRepository<SocialMedia, Long> {
+}
