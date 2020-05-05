6b24cf4602baf5633e6f40dbfb7fe89e50fa6cea
Byeonghun Kim <shyun823@gmail.com>
2020-05-06 01:34:04 +0900

    InterestsRepository 생성

---
+++ src/main/java/com/byeonghun/app/repository/InterestsRepository.java
@@ -0,0 +1,7 @@
+package com.byeonghun.app.repository;
+
+import com.byeonghun.app.entity.Interests;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InterestsRepository extends JpaRepository<Interests, Long> {
+}
